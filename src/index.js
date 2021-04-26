2ef11008758b38b6191aedaa28cb4b399dc0a804
Andrzej Jabłoński <andrzej@audioszum.pl>
2021-04-26 18:27:02 +0200

    added placeholder to input

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 // eslint-disable-next-line no-undef
-const { checkCardNumber } = require("./checkCardNumber");
+const { checkCardNumber } = require('./checkCardNumber');
 
-document.addEventListener("DOMContentLoaded", () => {
-    const inputData = document.querySelector(".card-checker__input--js");
+document.addEventListener('DOMContentLoaded', () => {
+
+    const inputData = document.querySelector('.card-checker__input--js');
     const info = document.querySelector(".card-checker__info-bar__content--js");
     const btn = document.querySelector(".card-checker__button--js");
 
-    btn.addEventListener("click", showCheckCard);
+    btn.addEventListener('click', showCheckCard);
+    inputData.addEventListener('click', () => {
+        inputData.value = '';
+    });
 
-    function showCheckCard(event) {
+    function showCheckCard(event){
         event.preventDefault();
         info.textContent = `Your card is: ${checkCardNumber(inputData.value)}`;
     }
-});
+})
